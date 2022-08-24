b47650b1fc7a251aad99422ef31110d0dd71dc04
Md Nayem Hossain <nayemhossainmd1@gmail.com>
2022-08-24 14:00:14 +0600

    stack: infix to prefix

---
+++ stack/infixToPrefix.cpp
@@ -0,0 +1,136 @@
+#include <bits/stdc++.h>
+#include "MYSTACK.h"
+
+using namespace std;
+
+int precisionCalc(char c)
+{
+    if (c == '^')
+    {
+        return 3;
+    }
+    else if (c == '*' || c == '/')
+    {
+        return 2;
+    }
+    else if (c == '+' || c == '-')
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+string infixToPrefix(string chk)
+{
+    reverse(chk.begin(), chk.end());
+    Stack<char> st;
+    string result;
+
+    for (int i = 0; i < chk.length(); i++)
+    {
+        if (chk[i] >= '0' && chk[i] <= '9')
+        {
+            result += chk[i];
+        }
+        else if (chk[i] == ')')
+        {
+            st.push(chk[i]);
+        }
+        else if (chk[i] == '(')
+        {
+            while (!st.empty() && st.Top() != ')')
+            {
+                result += st.pop();
+            }
+            if (!st.empty())
+            {
+                st.pop();
+            }
+        }
+        else
+        {
+            while (!st.empty() && precisionCalc(st.Top()) >= precisionCalc(chk[i]))
+            {
+                result += st.pop();
+            }
+            st.push(chk[i]);
+        }
+    }
+
+    while (!st.empty())
+    {
+        result += st.pop();
+    }
+
+    reverse(result.begin(), result.end());
+
+    return result;
+}
+
+int prefixEvaluation(string chk)
+{
+    Stack<int> st;
+
+    for (int i = chk.length() - 1; i >= 0; i--)
+    {
+        if (chk[i] >= '0' && chk[i] <= '9')
+        {
+            st.push(chk[i] - '0');
+        }
+        else
+        {
+            int a = st.pop();
+            int b = st.pop();
+
+            switch (chk[i])
+            {
+
+            case '+':
+                st.push(a + b);
+                break;
+
+            case '-':
+                st.push(a - b);
+                break;
+
+            case '*':
+                st.push(a * b);
+                break;
+
+            case '/':
+                st.push(a / b);
+                break;
+
+            case '^':
+                st.push(pow(a, b));
+                break;
+
+            default:
+                break;
+            }
+        }
+    }
+    return st.Top();
+}
+
+/*
++*423
+-+7*45+20
+
+*/
+
+int main()
+{
+    string infix = "(7+(4*5))-(2+0)";
+    string prefix;
+    prefix = infixToPrefix(infix);
+    cout << endl
+         << prefix << endl
+         << endl
+         << prefixEvaluation(prefix) << endl;
+
+    return 0;
+}
